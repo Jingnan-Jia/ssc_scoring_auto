0ba94fbc8233d13d193e337469db392cd268d55a
jjia <972931271@qq.com>
2021-07-01 17:08:47 +0200

    test a lot of 3DNets

--- tests/test_Nets.py
+++ tests/test_Nets.py
@@ -7,43 +7,44 @@ from monai.networks import eval_mode
 from tests.utils import test_script_save
 import unittest
 from parameterized import parameterized
-from ssc_scoring.run_pos import Cnn3fc1
+from ssc_scoring.run_pos import Cnn3fc1, Cnn3fc2, Cnn4fc2, Cnn5fc2, Cnn6fc2, Vgg11_3d
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 TEST_CASE_1 = [
-    {"num_classes": 5},
+    {"num_classes": 5, "base": 8},
     (4, 1, 192, 256, 256),
     (4, 5),
 ]
 
 TEST_CASE_2 = [
-    {"num_classes": 1},
+    {"num_classes": 1, "base": 8},
     (4, 1, 192, 256, 256),
     (4, 1),
 ]
 
 TEST_CASE_3 = [
-    {"num_classes": 9},
+    {"num_classes": 9, "base": 8},
     (4, 1, 192, 256, 256),
     (4, 9),
 ]
 
 CASES = [TEST_CASE_1, TEST_CASE_2, TEST_CASE_3]
 
-
-class TestCnn3fc1(unittest.TestCase):
+class TestNets(unittest.TestCase):
     @parameterized.expand(CASES)
     def test_shape(self, input_param, input_shape, expected_shape):
-        net = Cnn3fc1(**input_param).to(device)
-        with eval_mode(net):
-            result = net.forward(torch.randn(input_shape).to(device))
-            self.assertEqual(result.shape, expected_shape)
+        for Net in [Cnn3fc1, Cnn3fc2, Cnn4fc2, Cnn5fc2, Cnn6fc2, Vgg11_3d]:
+            net = Net(**input_param).to(device)
+            with eval_mode(net):
+                result = net.forward(torch.randn(input_shape).to(device))
+                self.assertEqual(result.shape, expected_shape)
 
     def test_script(self):
-        net = Cnn3fc1(num_classes=1).to(device)
         test_data = torch.randn(2, 1, 512, 256, 256)
-        test_script_save(net, test_data)
+        for Net in [Cnn3fc1, Cnn3fc2, Cnn4fc2, Cnn5fc2, Cnn6fc2, Vgg11_3d]:
+            net = Net(num_classes=8).to(device)
+            test_script_save(net, test_data)
 
 
 if __name__ == "__main__":
