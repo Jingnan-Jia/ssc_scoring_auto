1cfdf2732058ba1e54c332755a0fc73731dfe020
jjia <972931271@qq.com>
2021-07-10 13:38:12 +0200

    add shape test for randgausionnoiseposd

---
+++ tests/test_RandGaussianNoisePosd.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# @Time    : 7/10/21 1:21 PM
+# @Author  : Jingnan
+# @Email   : jiajingnan2222@gmail.com
+from ssc_scoring.mymodules.mytrans import RandGaussianNoisePosd
+
+import unittest
+import tempfile
+import os
+
+from parameterized import parameterized
+import numpy as np
+import myutil.myutil as futil
+from tests.utils import Compare
+
+
+TEST_CASE_1 = [
+    {"image_key": np.array([[0, 1], [1, 0]]).astype(np.float32)},  # this one will be changed
+
+     {"image_key": np.array([[0, 1], [1, 0]]).astype(np.float32)}, # this one will be changed
+]
+
+
+class TestRandGaussianNoisePosd(unittest.TestCase):
+    @parameterized.expand([TEST_CASE_1])
+    def test_RandGaussianNoisePosd_shape(self, input_data, expected_out):
+        result = RandGaussianNoisePosd()(input_data)
+        self.assertEqual(set(result.keys()), set(expected_out.keys()))
+        self.assertTrue(result['image_key'].shape, expected_out['image_key'].shape)
+
+if __name__ == "__main__":
+    unittest.main()
