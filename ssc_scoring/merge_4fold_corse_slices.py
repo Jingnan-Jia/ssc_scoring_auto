53f2b75d5f068cba4dce5f874fc74f2ed88430ea
jjia <972931271@qq.com>
2021-07-14 01:32:42 +0200

    fix copy bug

--- ssc_scoring/merge_4fold_corse_slices.py
+++ ssc_scoring/merge_4fold_corse_slices.py
@@ -17,14 +17,14 @@ if not os.path.isdir(all_dir):
 #     source_dir = PathPos(id=ex_id).id_dir + '/predicted_slices'
 #     file_names = os.listdir(source_dir)
 #     for file_name in file_names:
-#         shutil.move(os.path.join(source_dir, file_name), os.path.join(all_dir,file_name))
+#         shutil.copy(os.path.join(source_dir, file_name), os.path.join(all_dir,file_name))
 
 
 source_dir = PathPos().ori_data_dir
 source_dir = '/data/jjia/ssc_scoring/ssc_scoring/dataset/SSc_DeepLearning'
 print(source_dir)
 dir_names = os.listdir(source_dir)
-dir_names = [i for i in dir_names ]
+dir_names = [i for i in dir_names if 'Pat' in i]
 print(len(dir_names))
 for dir_name in dir_names:
     src_files = glob.glob(os.path.join(source_dir, dir_name, 'Level*_up.mha'))
@@ -42,4 +42,4 @@ for dir_name in dir_names:
         # print(tgt_file)
         if not os.path.isfile(tgt_file):
             print('to: ', tgt_file)
-            shutil.move(file, tgt_file )
+            shutil.copy(file, tgt_file )
