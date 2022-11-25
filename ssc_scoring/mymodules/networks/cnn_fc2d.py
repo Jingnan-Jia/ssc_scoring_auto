772f113d82637bba4f773d8dc3d50ee12c578fe2
Jingnan-Jia <jiajingnan2222@gmail.com>
2022-11-25 23:09:20 +0100

    ssc_scoring, jobid is 857188

--- ssc_scoring/mymodules/networks/cnn_fc2d.py
+++ ssc_scoring/mymodules/networks/cnn_fc2d.py
@@ -176,11 +176,11 @@ class vgg11_HR(nn.Module):
         self.features = nn.Sequential(
             nn.Conv2d(1, 64, kernel_size=11, stride=4, padding=2),
             nn.ReLU(inplace=True),
-            # nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),
 
             nn.Conv2d(64, 128, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
-            # nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),
 
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
